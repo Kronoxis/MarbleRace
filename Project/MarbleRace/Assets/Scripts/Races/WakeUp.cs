92c1417ec51f5c4d436996347b46329aab125432
ArcaneSpirits <arcanespiritsyt@gmail.com>
2017-02-05 18:20:54 +0100

    Renamed Folders Fixed Sleeping Actors

---
+++ Project/MarbleRace/Assets/Scripts/Races/WakeUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WakeUp : MonoBehaviour {
+
+    Rigidbody m_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_Rigidbody.WakeUp();
+	}
+}
